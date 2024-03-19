7ee1fb37902b593e10ed2837e95b792aa3d4d910
alexs <alexshalimov2019@gmail.com>
2024-03-19 09:37:06 +0200

    Task 1-2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//Напишіть програму, яка перевіряє пароль користувача на відповідність
+//вимогам безпеки (наприклад, мінімальна довжина, наявність цифр та
+//спеціальних символів). Використовуйте логічні оператори для перевірки
+//умов.
+
+
+//string password;
+//Console.WriteLine("Enter password");
+//password = Console.ReadLine();
+
+//if (password != null && password.Length >= 8)
+//{
+//    Console.WriteLine("Your password nice!");
+//}
+//else
+//{
+//    Console.WriteLine("Minimum 8 symbols");
+//}
+
+
+
+
+
+//Напишіть програму, яка перевіряє, чи є введене користувачем число
+//простим. Використовуйте арифметичні операції для перевірки дільників
+//числа.
+
+
+//while (true)
+//{
+//    uint number;
+//    bool prime=false;
+//    Console.WriteLine("Enter number!");
+//    number = uint.Parse(Console.ReadLine());
+//    for (int i = 2; i < number; i++)
+//    {
+//        if (number % i == 0)
+//        {
+//            Console.WriteLine("Number is not prime");
+//        }
+//        else
+//        {
+//            prime = true;
+//        }
+//    }
+//    if( prime )
+//    {
+//        Console.WriteLine("Number is prime");
+//    }
+//}
