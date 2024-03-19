d080c2797dcebc100828f436b7cd5947fb3116b6
alexs <alexshalimov2019@gmail.com>
2024-03-19 10:18:08 +0200

    Task 3-4 operator and Task 1-2 arrays

--- Program.cs
+++ Program.cs
@@ -1,73 +1,138 @@
-﻿//Напишіть програму, яка перевіряє пароль користувача на відповідність
-//вимогам безпеки (наприклад, мінімальна довжина, наявність цифр та
-//спеціальних символів). Використовуйте логічні оператори для перевірки
+﻿//Задачі на арифметичні та логічні оператори
+void Task1_operator() {
+//    Напишіть програму, яка перевіряє пароль користувача на відповідність
+//    вимогам безпеки(наприклад, мінімальна довжина, наявність цифр та
+//    спеціальних символів).Використовуйте логічні оператори для перевірки
 //умов.
 
 
-//string password;
-//Console.WriteLine("Enter password");
-//password = Console.ReadLine();
+string password;
+    Console.WriteLine("Enter password");
+    password = Console.ReadLine();
 
-//if (password != null && password.Length >= 8)
-//{
-//    Console.WriteLine("Your password nice!");
-//}
-//else
-//{
-//    Console.WriteLine("Minimum 8 symbols");
-//}  task1
+    if (password != null && password.Length >= 8)
+    {
+        Console.WriteLine("Your password nice!");
+    }
+    else
+    {
+        Console.WriteLine("Minimum 8 symbols");
+    }
+}
 
-
-
-//Напишіть програму, яка перевіряє, чи є введене користувачем число
+void Task2_operator() {
+//    Напишіть програму, яка перевіряє, чи є введене користувачем число
 //простим.Використовуйте арифметичні операції для перевірки дільників
 //числа.
 
 
-//while (true)
-//{
-//    uint number;
-//    bool prime = false;
-//    Console.WriteLine("Enter number!");
-//    number = uint.Parse(Console.ReadLine());
-//    for (int i = 2; i < number; i++)
-//    {
-//        if (number % i == 0)
-//        {
-//            Console.WriteLine("Number is not prime");
-//        }
-//        else
-//        {
-//            prime = true;
-//        }
-//    }
-//    if (prime)
-//    {
-//        Console.WriteLine("Number is prime");
-//    }
-//}
+while (true)
+    {
+        uint number;
+        bool prime = false;
+        Console.WriteLine("Enter number!");
+        number = uint.Parse(Console.ReadLine());
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                Console.WriteLine("Number is not prime");
+            }
+            else
+            {
+                prime = true;
+            }
+        }
+        if (prime)
+        {
+            Console.WriteLine("Number is prime");
+        }
+    }
+}
 
-    //Напишіть програму, яка перевіряє, чи належить введене користувачем
- //число заданому інтервалу. Використовуйте логічні оператори для перевірки
- //умови належності числа до інтервалу.
-
-
-    //int number;
-    //Console.WriteLine("Enter number");
-    //number = int.Parse(Console.ReadLine());
-    //int low, high;
-    //Console.WriteLine("Enter low border");
-    //low = int.Parse(Console.ReadLine());
-    //Console.WriteLine("Enter hight border");
-    //high = int.Parse(Console.ReadLine());
-    //if(number >= low && number <= high)
-    //{
-    //    Console.WriteLine("Number is in interval");
-    //}
-    //else
-    //{
-    //    Console.WriteLine("Number is not in interval");
-    //}
+void Task3_operator() {
+//    Напишіть програму, яка перевіряє, чи належить введене користувачем
+//число заданому інтервалу.Використовуйте логічні оператори для перевірки
+//умови належності числа до інтервалу.
 
 
+int number;
+    Console.WriteLine("Enter number");
+    number = int.Parse(Console.ReadLine());
+    int low, high;
+    Console.WriteLine("Enter low border");
+    low = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter hight border");
+    high = int.Parse(Console.ReadLine());
+    if (number >= low && number <= high)
+    {
+        Console.WriteLine("Number is in interval");
+    }
+    else
+    {
+        Console.WriteLine("Number is not in interval");
+    }
+}
 
+void Task4_operator()
+{
+    //    Напишіть програму, яка перевіряє, чи може існувати трикутник з
+    //введених користувачем довжин сторін.Використовуйте логічні оператори
+    //для перевірки умови існування трикутника.
+
+    int a, b, c;
+    Console.WriteLine("Enter a sides ");
+    a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter b sides ");
+    b = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter c sides ");
+    c = int.Parse(Console.ReadLine());
+
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        Console.WriteLine("Such a triangle has a right to exist");
+    }
+    else
+    {
+        Console.WriteLine("Such a triangle is not has the right to exist");
+    }
+}
+
+
+//Задачі на одновимірні та двовимірні масиви
+
+void Task1_arrays()
+{
+    //    Напишіть програму, яка знаходить: суму всіх елементів у заданому
+    //масиві.
+
+    int[] a= { 5, 4, 8, 9, 7, 5, 7, 4, 9 };
+
+    int sum = 0;
+
+    for(int i = 0; i < a.Length; i++)
+    {
+        sum += a[i];
+    }
+    Console.WriteLine($"Cума елементів масиву{sum}");
+
+}
+
+void Task2_arrays()
+{
+//     Напишіть програму, яка знаходить середнє значення елементів у
+//масиві.
+
+    int[] a = { 5, 4, 8, 9, 7, 5, 7, 4, 9 };
+
+    int sum = 0;
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        sum += a[i];
+    }
+
+    int midle = sum/a.Length;
+    Console.WriteLine($"Cереднэ значення елементів масиву{sum}");
+
+}
